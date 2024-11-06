02b03fda5578cb68dbd452ee765641426bd33404
iriska-gri <iriska190391@mail.ru>
2024-11-06 15:58:54 +0300

    Сборка приложения

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sys
-import design
+from design import Ui_MainWindow
 from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
 from testwork import WorkTest
@@ -8,33 +8,35 @@ from testwork import WorkTest
 # Если не будете использовать аргументы командной строки, QApplication([]) тоже работает
 
 
-class MainWindow(QMainWindow, design.Ui_MainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
+        self.buttonclass1.clicked.connect(self.the_button_was_clicked)
+        # self.setWindowTitle("My App")
+        # self.setFixedSize(QSize(400, 300))
+        # button = QPushButton("Press Me!")
 
-        self.setWindowTitle("My App")
-        self.setFixedSize(QSize(400, 300))
-        button = QPushButton("Press Me!")
+        # # Set the central widget of the Window.
+        # self.setCentralWidget(button)
+        # button.setCheckable(True)
+        # button.clicked.connect(self.the_button_was_clicked)
 
-        # Set the central widget of the Window.
-        self.setCentralWidget(button)
-        button.setCheckable(True)
-        button.clicked.connect(self.the_button_was_clicked)
-
-        # Set the central widget of the Window.
-        self.setCentralWidget(button)
+        # # Set the central widget of the Window.
+        # self.setCentralWidget(button)
 
     def the_button_was_clicked(self):
-        t= WorkTest()
-        t.settingssheet()
+        print('Создаем фрейм')
+        # t= WorkTest()
+        # t.settingssheet()
 
         
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
 
-app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
 
-window = MainWindow()
-window.show()
-
-app.exec()
+    app.exec()
 
     
